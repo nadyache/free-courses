7899fd88d2873f41654cef0fa13dc509c8a89650
ppp <solmy@list.ru>
2018-06-17 23:38:35 +0300

    add some

--- task3.js
+++ task3.js
@@ -1,3 +1,16 @@
-/* Task 3 */
-
-/* Put your code here */
+var
+    a = Number(process.argv[2]);
+    b = Number(process.argv[3]);
+    c = 0;
+    result = 0;
+function nod(a, b) {
+    if(b > 0) {
+        var c = a%b;
+        return nod(b, c);
+    }
+    else {
+        return Math.abs(a);
+    }
+}
+result = nod(a, b);
+process.stdout.write(result)
